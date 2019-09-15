d8cf9ce7643d9efde6c9fb4a716b927d78b376e5
KBGHM101 <55159535+KBGHM101@users.noreply.github.com>
2019-09-15 16:44:16 -0400

    Run Mecanum Wheels Programs

---
+++ MecanumWheels/MecanumWheels.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import static android.os.SystemClock.sleep;
+
+@TeleOp(name = "TestMecanumReg")
+@Disabled
+public class TestMecanum extends DMRelicAbstract {
+    public TestMecanum() {
+    }
+
+    @Override
+    public void init() {
+
+        super.init();
+
+        // Set all motors to run without encoders
+        motorRightA.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRightB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorLeftA.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorLeftB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        //parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+
+        //imu = hardwareMap.get(BNO055IMU.class, "imu");
+        //imu.initialize(parameters);
+
+        IncVal = 5; 
+ 
+    }
+ 
+    @Override
+    public void loop() {
+
+        super.loop();
+
+        // Set drive motor power
+        motorRightA.setPower(powerRightA);
+        motorRightB.setPower(powerRightB);
+        motorLeftA.setPower(powerLeftA);
+        motorLeftB.setPower(powerLeftB);
+
+        // Set controls
+        velocityDrive = -gamepad1.left_stick_y;
+        strafeDrive = gamepad1.left_stick_x;
+        rotationDrive = gamepad1.right_stick_x;
+
+       // Scale drive motor power for better control at low power
+        powerRightA = (float) scaleInput(powerRightA);
+        powerRightB = (float) scaleInput(powerRightB);
+        powerLeftA = (float) scaleInput(powerLeftA);
+        powerLeftB = (float) scaleInput(powerLeftB);
+
+        //Create dead-zone for drive train controls
+        if (gamepad1.left_stick_x <= 0.05 && gamepad1.left_stick_x >= -0.05) {
+            gamepad1.left_stick_x = 0;
+        }
+
+        if (gamepad1.left_stick_y <= 0.05 && gamepad1.left_stick_y >= -0.05) {
+            gamepad1.left_stick_y = 0;
+        }
+
+        if (gamepad1.right_stick_x <= 0.05 && gamepad1.right_stick_x >= -0.05) {
+            gamepad1.right_stick_x = 0;
+        }
+
+        // If the left stick and the right stick is used it halves the power  of the motors for better accuracy
+        if (gamepad1.left_stick_y > 0 || gamepad1.left_stick_y < 0 && gamepad1.right_stick_x > 0 || gamepad1.right_stick_x < 0) {
+ 
+
+            powerRightA = Range.clip(powerRightA, -0.5f, 0.5f);
+            powerRightB = Range.clip(powerRightB, -0.5f, 0.5f);
+            powerLeftA = Range.clip(powerLeftA, -0.5f, 0.5f);
+            powerLeftB = Range.clip(powerLeftB, -0.5f, 0.5f);
+
+        } else if (gamepad1.left_stick_x > 0 || gamepad1.left_stick_x < 0 && gamepad1.right_stick_x > 0 || gamepad1.right_stick_x < 0) {
+
+            powerRightA = Range.clip(powerRightA, -0.5f, 0.5f);
+            powerRightB = Range.clip(powerRightB, -0.5f, 0.5f);
+            powerLeftA = Range.clip(powerLeftA, -0.5f, 0.5f);
+            powerLeftB = Range.clip(powerLeftB, -0.5f, 0.5f);
+
+        } else {
+
+            powerRightA = Range.clip(powerRightA, -1, 1);
+            powerRightB = Range.clip(powerRightB, -1, 1);
+            powerLeftA = Range.clip(powerLeftA, -1, 1);
+            powerLeftB = Range.clip(powerLeftB, -1, 1);
+
+        }
+
+        powerRightA = velocityDrive + rotationDrive + strafeDrive;
+        powerRightB = velocityDrive + rotationDrive - strafeDrive;
+        powerLeftA = velocityDrive - rotationDrive - strafeDrive;
+        powerLeftB = velocityDrive - rotationDrive + strafeDrive;
+
+
+        telemetry.addData("RightA: ",  powerRightA);
+        telemetry.addData("RightB: ",  powerRightB);
+        telemetry.addData("LeftA: ",  powerLeftA);
+        telemetry.addData("LeftB: ",  powerLeftB);
+        telemetry.update();
+// End OpMode Loop Method
+    }
+    @Override
+    public void stop ()
+    {
+        super.stop();
+    }
+}
